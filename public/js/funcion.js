9963af8edcc47b98c85ff970377da95f68563e72
Juan Carlos Carrera <carrera1995@hotmail.es>
2018-09-10 10:24:47 -0500

    primer commit

---
+++ public/js/funcion.js
@@ -0,0 +1,9 @@
+
+        $(document).ready(function(){
+            console.log("Script working properly");
+        });
+        
+        $("#tagline").click(function(){
+            alert("The paragraph was clicked.");
+        });
+    
---
+++ src/Controller/WebController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('web/home.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/productos", name="productos")
+     */
+    public function productos()
+    {
+        return $this->render('web/productos.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/contactenos", name="contactenos")
+     */
+    public function contactenos()
+    {
+        return $this->render('web/contactenos.html.twig', [
+            'controller_name' => 'WebController',
+        ]);
+    }
+
+
+
+
+}
